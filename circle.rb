4018f10829d1d938e9d22d667f25e0dd838c8302
Matt Hauer <matthew.hauer@gmail.com>
2015-02-19 18:52:07 -0800

    I figured out how to use pi!

--- circle.rb
+++ circle.rb
@@ -1,6 +1,6 @@
 puts "Let's make a circle!\n"
 puts "Enter the radius: "
 r = gets.chomp.to_f
-a = 3.1453 * r**2
-c = 2 * r * 3.1453
+a = Math::PI * r**2
+c = 2 * r * Math::PI
 puts "A circle with raidus "+r.to_s+" has an area of "+a.to_s+" and a circumfrence of "+c.to_s+"\n"
