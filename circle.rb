c91494233753aec88971b0bcc796dd8e34264666
Matt Hauer <matthew.hauer@gmail.com>
2015-02-19 19:08:59 -0800

    Added basic logical error checking for the circle radius

--- circle.rb
+++ circle.rb
@@ -3,4 +3,8 @@ puts "Enter the radius: "
 r = gets.chomp.to_f
 a = Math::PI * r**2
 c = 2 * r * Math::PI
-puts "A circle with raidus "+r.to_s+" has an area of "+a.to_s+" and a circumfrence of "+c.to_s+"\n"
+if r <= 0.0
+	puts "Error, the radius must be a positive number"
+else
+	puts "A circle with raidus "+r.to_s+" has an area of "+a.to_s+" and a circumfrence of "+c.to_s+"\n"
+end
